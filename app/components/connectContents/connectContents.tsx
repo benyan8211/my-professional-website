8f7a7adff1ba4ebfb2039841d7d5bb8e5b9bc675
Benjamin Yan <benjaminyan@Benjamins-MacBook-Pro.local>
2026-05-14 10:52:14 -0400

    Refine wording on Connect page

--- app/components/connectContents/connectContents.tsx
+++ app/components/connectContents/connectContents.tsx
@@ -6,7 +6,7 @@ import ClickableCard from '../clickableCard/clickableCard';
 const ConnectContents = () => {
     return (
         <div className="grid grid-cols-3 gap-4 p-4 justify-items-center mt-30">
-            <ClickableCard image={linkedinLogo} text={'Connect with me on LinkedIn'} url={'https://www.linkedin.com/in/benjamin-yan-b499b185/'} />
+            <ClickableCard image={linkedinLogo} text={'Add me on LinkedIn'} url={'https://www.linkedin.com/in/benjamin-yan-b499b185/'} />
             <ClickableCard image={githubLogoLarge} text={'Follow me on GitHub'} url={'https://github.com/benyan8211'} />
             <ClickableCard image={gmailLogo} text={'Send me an email'} url={'mailto:benyan8211@gmail.com'}/>
         </div>
