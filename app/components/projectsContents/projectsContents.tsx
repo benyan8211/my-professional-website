29d0d40598603872b8bae663ffe09799592f0f4d
Benjamin Yan <benjaminyan@Benjamins-MacBook-Pro.local>
2026-08-26 21:45:04 -0400

    Add Sales Email Scribe project to Projects Page

--- app/components/projectsContents/projectsContents.tsx
+++ app/components/projectsContents/projectsContents.tsx
@@ -3,21 +3,30 @@
 import Image from 'next/image'
 import websiteUnderConstruction from "../../assets/website_under_construction.png";
 import styles from "./projectsContents.module.css";
+import { Card } from "flowbite-react";
+import salesEmailScribeLogo from "../../assets/sales_email_scribe_logo.png";
 
 const ProjectsContents = () => {
+    const handleCardNavigation = (url: string) => {
+        // ✅ Opens the URL cleanly in a new tab
+        window.open(url, "_blank", "noopener,noreferrer");
+    };
     return (
-        <>
-            <div className="flex justify-center mt-15">
-                <Image
-                    src={websiteUnderConstruction}
-                    width={500}
-                    height={500}
-                    alt="Picture of aws logo"
-                    className="object-cover"
-                />
-            </div>
-            <div className={styles.projectsText}>This page is under construction. Please come back later.</div>
-        </>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 max-w-5xl mx-auto p-6 ml-85">
+            <Card
+                className="max-w-sm cursor-pointer border border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 transition-shadow duration-300 ease-out hover:shadow-[0_0_18px_rgba(255,255,255,0.95)]"
+                imgAlt="Meaningful alt text for an image that is not purely decorative"
+                imgSrc={salesEmailScribeLogo.src}
+                onClick={() => handleCardNavigation("https://sales-email-scribe.onrender.com")}
+            >
+                <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    Sales Email Scribe
+                </h5>
+                <p className="font-normal text-gray-700 dark:text-gray-400">
+                    An AI powered scribe that writes high quality, custom tailored sales emails.
+                </p>
+            </Card>
+        </div>
     );
 }
 
