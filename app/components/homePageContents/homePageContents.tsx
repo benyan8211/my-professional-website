910277a493c16ddd75011a16c344bb2566d0a712
Benjamin Yan <benjaminyan@Benjamins-MacBook-Pro.local>
2026-08-26 20:32:16 -0400

    Move homepage content slightly left

--- app/components/homePageContents/homePageContents.tsx
+++ app/components/homePageContents/homePageContents.tsx
@@ -7,7 +7,7 @@ import styles from "./homePageContents.module.css";
 const HomePageContents = () => {
     return (
         <>
-            <div className="flex justify-center mt-28">
+            <div className="flex justify-center mt-28 ml-3">
                 <div className="relative w-82 h-82 overflow-hidden rounded-full">
                     <Image
                         src={myProfessionalPicture}
