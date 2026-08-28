fd3f3a4f5856d5817b8031a0f667f5f183a31d8b
Benjamin Yan <benjaminyan@Benjamins-MacBook-Pro.local>
2026-08-28 10:43:28 -0400

    Add css to disable thick ring appearing after clicking download resume button

--- app/components/aboutMeContents/aboutMeContents.tsx
+++ app/components/aboutMeContents/aboutMeContents.tsx
@@ -117,7 +117,7 @@ const AboutMeContents = () => {
                 <div className={styles.resumeText}>For a detailed look at my professional history and technical skills:</div>
                 <Button 
                     color="dark"
-                    className="whitespace-nowrap justify-self-center mb-10 cursor-pointer w-50 h-18 rounded-lg border border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 transition-all duration-300 ease-out hover:border-white dark:hover:border-white hover:shadow-[0_0_12px_rgba(255,255,255,1),0_0_30px_rgba(255,255,255,0.6)]"
+                    className="!ring-0 whitespace-nowrap justify-self-center mb-10 cursor-pointer w-50 h-18 rounded-lg border border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 transition-all duration-300 ease-out hover:border-white dark:hover:border-white hover:shadow-[0_0_12px_rgba(255,255,255,1),0_0_30px_rgba(255,255,255,0.6)]"
                     onClick={handleDownload}
                 >
                     <Image
